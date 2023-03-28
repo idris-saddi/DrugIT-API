b18176ca5087392a9b11715583871b3ca79076f1
IdrisOmit <saddi.idris@gmail.com>
2023-03-28 23:26:54 +0100

    add target entity

---
+++ src/target/entity/target.entity.ts
@@ -0,0 +1,35 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  Entity,
+} from 'typeorm';
+
+@Entity()
+export class Target {
+  @PrimaryGeneratedColumn({})
+  id: string;
+
+  @Column({})
+  name: string;
+
+  @Column({})
+  description: string;
+
+  @CreateDateColumn({
+    nullable: false,
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    nullable: true,
+  })
+  updated_at: Date;
+
+  @DeleteDateColumn({
+    nullable: true,
+  })
+  deleted_at: Date;
+}
--- src/target/target.module.ts
+++ src/target/target.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TargetController } from './target.controller';
 import { TargetService } from './target.service';
+import { Target } from './entity/target.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Target]),
+  ],
   controllers: [TargetController],
   providers: [TargetService]
 })
